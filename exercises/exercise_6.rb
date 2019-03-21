b4d06123d9a341f5fbc5cd253cd881a7e422d45e
Lauren <laurenribeiro06@gmail.com>
2019-03-21 21:38:48 +0000

    Add one-to-many associations, create employees for stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Lauren", last_name: "Smith", hourly_rate: 40)
+@store1.employees.create(first_name: "KJ", last_name: "Sandwich", hourly_rate: 80)
+
+@store2.employees.create(first_name: "Danielle", last_name: "Schmidt", hourly_rate: 80)
+@store2.employees.create(first_name: "Gianni", last_name: "Michone", hourly_rate: 70)
+@store2.employees.create(first_name: "Yoland", last_name: "Sam", hourly_rate: 50)
