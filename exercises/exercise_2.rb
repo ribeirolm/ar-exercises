1dfada9d81e984e12ded2409f4a9d1a11633a30a
Lauren <laurenribeiro06@gmail.com>
2019-03-21 20:52:42 +0000

    Add exercise 2 answer, update first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store1.name = "New phone who dis"
+
+@store2 = Store.find_by(id: 2)
+
+# puts Store.first
+# puts Store.second
